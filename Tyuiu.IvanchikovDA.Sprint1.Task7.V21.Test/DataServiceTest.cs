eb256be53da9ad547e3128364900fa416f5ae30b
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-17 03:15:26 +0500

    Task7V21

---
+++ Tyuiu.IvanchikovDA.Sprint1.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.IvanchikovDA.Sprint1.Task7.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
